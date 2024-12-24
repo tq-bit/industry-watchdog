b9dc55def495514c7052e784598d74f56cc14edf
Tq-bit <t.quante@outlook.com>
2024-12-24 09:27:02 +0100

    chore: add qbit logo

--- routes/_app.tsx
+++ routes/_app.tsx
@@ -6,6 +6,7 @@ export default function App({ Component }: PageProps) {
       <head>
         <meta charset="utf-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <link rel="icon" href="/logo.png" type="image/png" sizes="32x32"/>
         <title>Industry Watchdog</title>
         <link rel="stylesheet" href="/styles.css" />
       </head>
